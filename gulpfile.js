5a9452eae669d613f865c6bbd98d756c21344e5a
JianLongMiao <287449943@qq.com>
2016-04-14 13:20:21 +0800

    Added files via upload

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+﻿/// <binding Clean='clean' />
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    fs = require("fs"),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+
+var rootPath = './wwwroot/';
+var allJsPaths = [
+    rootPath + 'src/fImageView.js',
+    rootPath + 'src/utils.js',
+    rootPath + 'src/ieRotate.js',
+    rootPath + 'src/fimage.js',
+    rootPath + 'src/fImagePanel.js'
+];
+gulp.task('buildjs', function () {
+    return gulp.src(allJsPaths)
+        .pipe(concat('fImageView.js'))
+        .pipe(gulp.dest(rootPath + 'dist/'));
+});
+gulp.task('buildjsmin', function () {
+    return gulp.src(allJsPaths)
+        .pipe(uglify())
+        .pipe(concat('fImageView.min.js'))
+        .pipe(gulp.dest(rootPath + 'dist/'));
+});
+gulp.task('build', ['buildjs', 'buildjsmin']);
